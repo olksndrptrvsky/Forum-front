cd530dcef5f9fc0cb0379785f5a0d369a8998c22
Alexandr Petrovkiy <supchik.cn@gmail.com>
2020-07-06 22:17:22 +0300

    removed error handling

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
     userCred.password = password;
     this.logout();
     this.http.post<User>(this.userUrl + '/login', JSON.stringify(userCred) )
-      .pipe(catchError(this.handleError<User>('login'))).subscribe(user =>
+      .subscribe(user =>
     {
       if (user!= null)
       {
@@ -53,17 +53,4 @@ export class AuthService {
   }
 
 
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      console.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
 }
